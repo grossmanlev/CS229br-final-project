af9ad9de052f98c20555384400fd24476e3f35fb
grossmanlev <lgrossman@college.harvard.edu>
2019-04-10 21:23:49 -0400

    adding main.py with TODOs

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Simulation of Offspring Sex Ratio Evolution
+
+Lev Grossman, Anirudh Suresh
+CS229br -- Biology and Complexity
+"""
+
+
+"""
+TODOs:
+1) Code up the simple model found in Wild and West (great author names)
+    a) Birth
+    b) Maternal Investment
+    c) Dispersal
+    d) Male-Male Competition
+    e) Female-Female Competition
+2) Extend, alter the model??
+"""
